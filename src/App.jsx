ed15b7a9baa04dd3b858a2d8054265de8b09a8f2
Yumin <yjang2022@fau.edu>
2026-02-28 04:03:22 -0500

    Step 2: Make the query with the user input, create an API call, and display your screenshot

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,95 @@ import APIForm from './Components/APIForm';
 
 const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
 
-const [inputs, setInputs] = useState({
-  url: "",
-  format: "",
-  no_ads: "",
-  no_cookie_banners: "",
-  width: "",
-  height: "",
-});
-
-const submitForm = () => {
-  
-};
-
 function App() {
+
+  const [inputs, setInputs] = useState({
+    url: "",
+    format: "",
+    no_ads: "",
+    no_cookie_banners: "",
+    width: "",
+    height: "",
+  });
+
+  const [currentImage, setCurrentImage] = useState(null);
+
+  const reset = () => {
+    setInputs({
+      url: "",
+      format: "",
+      no_ads: "",
+      no_cookie_banners: "",
+      width: "",
+      height: "",
+    });
+  };
+
+  const callAPI = async (query) => {
+    const response = await fetch(query);
+    const json = await response.json();
+
+    if (json.url == null) {
+      alert("Oops! Something went wrong with that query, let's try again!");
+    } else {
+      setCurrentImage(json.url);
+      reset();
+    }
+  };
+
+  const makeQuery = () => {
+    let wait_until = "network_idle";
+    let response_type = "json";
+    let fail_on_status = "400%2C404%2C500-511";
+
+    let fullURL = "https://" + inputs.url;
+
+    let query = `https://api.apiflash.com/v1/urltoimage?access_key=${ACCESS_KEY}
+    &url=${fullURL}
+    &format=${inputs.format}
+    &width=${inputs.width}
+    &height=${inputs.height}
+    &no_cookie_banners=${inputs.no_cookie_banners}
+    &no_ads=${inputs.no_ads}
+    &wait_until=${wait_until}
+    &response_type=${response_type}
+    &fail_on_status=${fail_on_status}`;
+
+    query = query.replace(/\s+/g, "");
+
+    callAPI(query).catch(console.error);
+  };
+
+  const submitForm = () => {
+    let defaultValues = {
+      format: "jpeg",
+      no_ads: "true",
+      no_cookie_banners: "true",
+      width: "1920",
+      height: "1080",
+    };
+
+    if (inputs.url === "") {
+      alert("You forgot to submit an url!");
+      return;
+    }
+
+    const updatedInputs = { ...inputs };
+
+    for (const [key, value] of Object.entries(inputs)) {
+      if (value === "") {
+        updatedInputs[key] = defaultValues[key];
+      }
+    }
+
+    setInputs(updatedInputs);
+    makeQuery();
+  };
+
   return (
     <div className="whole-page">
       <h1>Build Your Own Screenshot! 📸</h1>
-      
+
       <APIForm
         inputs={inputs}
         handleChange={(e) =>
@@ -32,10 +103,29 @@ function App() {
         }
         onSubmit={submitForm}
       />
-      <br></br>
+
+      <br />
+
+      {currentImage ? (
+        <img className="screenshot" src={currentImage} />
+      ) : (
+        <div></div>
+      )}
+
+      <div className="container">
+        <h3>Current Query Status:</h3>
+        <p>
+          &url={inputs.url} <br />
+          &format={inputs.format} <br />
+          &width={inputs.width} <br />
+          &height={inputs.height} <br />
+          &no_cookie_banners={inputs.no_cookie_banners} <br />
+          &no_ads={inputs.no_ads}
+        </p>
+      </div>
 
     </div>
   );
 }
 
-export default App
+export default App;
