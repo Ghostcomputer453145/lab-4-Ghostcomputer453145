32fa7afba3f2d8b07784d5b62d514f3ed1790aac
Yumin <yjang2022@fau.edu>
2026-02-28 01:08:18 -0500

    Step 1: Create your form for user input on the screenshot attributes

---
+++ src/components/APIForm.jsx
@@ -0,0 +1,40 @@
+const inputsInfo = [
+  "Input a link to any website you would like to take a screenshot of. Do not include https or any protocol in the URL",
+  "Input which image format you would prefer for your screenshot: jpeg, png, or webp",
+  "Input true or false if you would like your website screenshot to not contain any ads",
+  "Input true or false if you would like your website screenshot to not contain of those annoying 'allow cookies' banners",
+  "Choose the width of your screenshot (in pixels)",
+  "Choose the height of your screenshot (in pixels)",
+];
+
+const APIForm = ({ inputs, handleChange, onSubmit }) => {
+    return (
+    <div>
+        <h2>Select Your Image Attributes:</h2>
+
+        <form className="form-container">
+            {Object.entries(inputs).map(([key, value], index) => (
+                <li className="form" key={index}>
+                    <h3>{key}</h3>
+
+                    <input
+                    type="text"
+                    name={key}
+                    value={value}
+                    onChange={handleChange}
+                    className="textbox"
+                    />
+                    
+                    <p>{inputsInfo[index]}</p>
+                </li>
+            ))}
+        </form>
+
+        <button className="button" type="submit" onClick={onSubmit}>
+            Take that Pic! 🎞
+        </button>
+    </div>
+    );
+};
+
+export default APIForm;
